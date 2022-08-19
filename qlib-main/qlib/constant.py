d02e8425f8812b77be01fcd07ddd6556a57c5eb3
DuanyangZhang <734798060@qq.com>
2022-08-19 21:36:14 +0800

    initial commit

---
+++ qlib-main/qlib/constant.py
@@ -0,0 +1,13 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# REGION CONST
+REG_CN = "cn"
+REG_US = "us"
+REG_TW = "tw"
+
+# Epsilon for avoiding division by zero.
+EPS = 1e-12
+
+# Infinity in integer
+INF = 10**18
