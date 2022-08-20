c873d79803d079ff152bfbd4c8f24279d57157bd
DuanyangZhang <734798060@qq.com>
2022-08-20 18:25:58 +0800

    新增指数数据

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import qlib
-# region in [REG_CN, REG_US]
 from qlib.constant import REG_CN
-provider_uri = "/data/qlib_data/cn_data"  # target_dir
+provider_uri = "/data/qlib_data/cn_data_ak"  # target_dir
 qlib.init(provider_uri=provider_uri, region=REG_CN)
