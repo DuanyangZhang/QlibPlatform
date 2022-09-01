674318d881cf4425cde0993625609f11c15fe445
DuanyangZhang <734798060@qq.com>
2022-09-01 21:45:36 +0800

    debug数据

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import qlib
-from qlib.constant import REG_CN
-provider_uri = "/data/qlib_data/cn_data_ak"  # target_dir
-qlib.init(provider_uri=provider_uri, region=REG_CN)
+import torch
+
+print(torch.cuda.is_available())
